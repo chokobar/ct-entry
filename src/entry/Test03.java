b6b3db77ba987d285de3fd23397ed90e6b678933
chokobar89 <chokobar89@naver.com>
2025-04-09 21:57:37 +0900

    Initial commit

---
+++ src/entry/Test03.java
@@ -0,0 +1,12 @@
+package entry;
+
+public class Test03 {
+
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int age) {
+                return 2022 - age + 1;
+            }
+        }
+    }
+}
